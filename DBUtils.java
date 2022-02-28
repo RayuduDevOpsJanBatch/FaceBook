b0b57a08194fe811aea5a5fff2610b78573387f7
Rayudu <subbarayudu.net@gmail.com>
2022-02-28 20:57:30 +0530

    commiting Revert "updated"
    This reverts commit 67259ba4982261c17b81ac55f01eb5a5b6d9869c.

--- DBUtils.java
+++ DBUtils.java
@@ -1,7 +1,4 @@
-
-I'm adding this line, today Raghu also join with us
-
-	Hi Ravi kumar,
-   	I'm Rayudu. 
-   	adding additional lines to verify.
-   	2nd time i'm adding additional line
+Hi Ravi kumar,
+   I'm Rayudu. 
+   adding additional lines to verify.
+   2nd time i'm adding additional line
